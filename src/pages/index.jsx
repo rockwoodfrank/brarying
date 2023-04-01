f76fb17d9dfdc0934ce73037e36821d638d453b5
Rockwood Frank <rockwoodfrank@github.com>
2023-03-31 20:13:47 -0700

    Change to Poppins font

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import {useState,useEffect} from 'react';
 import Image from 'next/image';
+import Head from 'next/head';
 import Background from './background';
 import Map from './map';
 import SideBar from './sidebar';
@@ -10,12 +11,21 @@ export default function MyApp() {
     ];
     const [currentFloor, setCurrent] = useState(2);
     return (
-        <main>
-            <Background />
-            <Header index={currentFloor} floorList={floors}/>
-            <Map floor={currentFloor} />
-            <SideBar floors={floors} setter={setCurrent} current={currentFloor} />
-        </main>
+        <>
+            <Head>
+                <title>Anybody Brarying?</title>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
+            </Head>
+            <main>
+                
+                <Background />
+                <Header index={currentFloor} floorList={floors}/>
+                <Map floor={currentFloor} />
+                <SideBar floors={floors} setter={setCurrent} current={currentFloor} />
+            </main>
+        </>
     );
 }
 
